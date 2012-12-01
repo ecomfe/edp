9d9ab6efab02595fa2dcc5d6a921fc7f3cbf563a
erik <erik168@163.com>
2012-12-01 19:54:33 +0800

    add comment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require( './lib/edp' );
+/**
+ * @file package主脚本
+ * @author errorrik[errorrik@gmail.com]
+ */
+
+
+var edp = require( './lib/edp' );
+
+/**
+ * 内建模块
+ *
+ * @const
+ * @type {Array}
+ */
+var BUILT_IN = [
+    'config'
+];
+
+// 装载内建模块
+var modules = [];
+for ( var i = 0; i < BUILT_IN.length; i++ ) {
+    var moduleName = BUILT_IN[ i ];
+    edp.addModule(
+        require( './lib/' + moduleName )
+    );
+}
+
+module.exports = edp;
