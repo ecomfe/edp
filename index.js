0e734e285c2844ef14784b4b074fe15f7fef81a0
erik <erik168@163.com>
2012-12-08 15:16:46 +0800

    let index.js to load user modules

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var edp = require( './lib/edp' );
  * 内建模块
  *
  * @const
+ * @ignore
  * @type {Array}
  */
 var BUILT_IN = [
@@ -17,12 +18,35 @@ var BUILT_IN = [
 ];
 
 // 装载内建模块
-var modules = [];
-for ( var i = 0; i < BUILT_IN.length; i++ ) {
-    var moduleName = BUILT_IN[ i ];
-    edp.addModule(
-        require( './lib/' + moduleName )
-    );
-}
+( function () {
+    for ( var i = 0; i < BUILT_IN.length; i++ ) {
+        var moduleName = BUILT_IN[ i ];
+        edp.addModule(
+            require( './lib/' + moduleName )
+        );
+    }
+} )();
+
+// 装载用户模块
+( function () {
+    var fs = require( 'fs' );
+    var path = process.env.HOME + '/edp_modules';
+
+    if ( fs.existsSync( path ) && fs.statSync( path ).isDirectory() ) {
+        var files = fs.readdirSync( path );
+
+        files.forEach( 
+            function ( file ) {
+                file = path + '/' + file;
+                
+                if ( fs.statSync( file ).isFile() ) {
+                    var module = require( file );
+                    edp.addModule( module );
+                }
+            } 
+        );
+    }
+} )()
+
 
 module.exports = edp;
