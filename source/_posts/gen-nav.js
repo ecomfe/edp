4f5fe7099f464070d86dfa8dcdc153f898d2dabe
wangshuonpu <wangshuonpu@163.com>
2015-05-22 13:39:44 +0800

    fix sidebar

--- source/_posts/gen-nav.js
+++ source/_posts/gen-nav.js
@@ -34,12 +34,12 @@ files.forEach(function (file) {
 
 var types = [
     {name: 'Initialization', title: '安装与升级'},
-    {name: 'Package-management', title: '包管理'},
-    {name: 'Project-management', title: '项目管理'},
+    {name: 'Package_management', title: '包管理'},
+    {name: 'Project_management', title: '项目管理'},
     {name: 'Build', title: '项目构建'},
     {name: 'Lint', title: '前端代码静态检测'},
     {name: 'WebServer', title: '使用WebServer进行调试'},
-    {name: 'Command-extension', title: '扩展自己的EDP命令'},
+    {name: 'Command_extension', title: '扩展自己的EDP命令'},
     {name: 'Doctor', title: '诊断工具'}
 ];
 
@@ -48,10 +48,10 @@ types.forEach(function (type) {
     typesIdx[type.name] = type;
 });
 
-
 fileMetadatas.forEach(function (metadata) {
 
     var type = typesIdx[metadata.category];
+
     if (!type.docs) {
         type.docs = [];
     }
@@ -61,21 +61,22 @@ fileMetadatas.forEach(function (metadata) {
 
 var text = '';
 types.forEach(function (type) {
+    var categroyPath = type.name.replace(/_/g, '-');
     text += '\n<li><a class="cat-title">' + type.title + '</a>\n    <ul>';
     type.docs.forEach(function (doc) {
-        //text += '\n        <li><a href="<%= root_path %>/' + type.name + '/' + doc.filename + '/">' + doc.title + '</a></li>';
+        //text += '\n        <li><a href="<%= root_path %>/' + path + '/' + doc.filename + '/">' + doc.title + '</a></li>';
 
         if (doc.primaries.length > 0) {
-            text += '\n        <li><a class="cat-title" href="<%= root_path %>/' + type.name + '/' + doc.filename + '/">' + doc.title + '</a></li>';
+            text += '\n        <li><a class="cat-title" href="<%= root_path %>/' + categroyPath + '/' + doc.filename + '/">' + doc.title + '</a>';
             text += '\n        <ul>';
 
             doc.primaries.forEach(function (primary) {
-                text += '\n            <li><a href="<%= root_path %>/' + type.name + '/' + doc.filename + '/#' + primary + '">' + primary + '</a></li>';
+                text += '\n            <li><a href="<%= root_path %>/' + categroyPath + '/' + doc.filename + '/#' + primary + '">' + primary + '</a></li>';
             });
-            text += '\n        </ul>';
+            text += '\n        </ul>\n</li>';
         }
         else {
-            text += '\n        <li><a href="<%= root_path %>/' + type.name + '/' + doc.filename + '/">' + doc.title + '</a></li>';
+            text += '\n        <li><a href="<%= root_path %>/' + categroyPath + '/' + doc.filename + '/">' + doc.title + '</a></li>';
         }
     });
     text += '\n    </ul>\n</li>';
